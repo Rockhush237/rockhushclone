101c641e17122bf688a445e49a8f6db282ff736b
Rockhush237 <sbsvickysharma@gmail.com>
2025-07-29 16:18:09 +0530

    𝐫𝐨𝐜𝐤𝐡𝐮𝐬𝐡

---
+++ ROCKHUSH/plugins/bot/repo.py
@@ -0,0 +1,40 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ROCKHUSH import app
+
+BOT_USERNAME = "khushimusix_Robot"
+
+start_txt = """**
+✪ 𝐊𝐇𝐔𝐃 𝐁𝐀𝐍𝐀 𝐁𝐇𝐎𝐒𝐃𝐈𝐊𝐄 ✪
+ 
+**"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+     
+            [ 
+            InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ʙᴀʙᴇs✪", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            ],
+     
+            [
+             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/its_deva_heree"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/rockhushh"),
+             ],
+     
+             [
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/rockhushh"),
+             ],
+     
+              ]
+ 
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_video(
+        video="https://envs.sh/ozv.mp4",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
